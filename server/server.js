c50537a61a8bd9cf7b171bc89d63f28b48334081
yellowHuang <hlady1115@gmail.com>
2021-11-24 21:47:36 +0800

    add: server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const { gql } = require('apollo-server'); // parse this schema
+const { ApolloServer, gql } = require('apollo-server'); // parse this schema
 // gql is a tag function
 
 // def schema
@@ -14,3 +14,9 @@ const resolvers = {
     greeting: () => 'Hello GraphQL world',
   },
 };
+
+// server
+const server = new ApolloServer({ typeDefs, resolvers });
+server
+  .listen({ port: 9000 })
+  .then(({ url }) => console.log(`Server running at ${url}`));
